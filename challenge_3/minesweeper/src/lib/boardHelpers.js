3fab02e90993a923ad3ec509ff72b1632248eef5
Josh Alamillo <josh.alamillo@gmail.com>
2018-04-06 12:50:29 -0700

    completed a function that generates a board of any size

--- challenge_3/minesweeper/src/lib/boardHelpers.js
+++ challenge_3/minesweeper/src/lib/boardHelpers.js
@@ -59,3 +59,61 @@ const handleGridClick = (grid, squareParams) => {
   
 }
 
+const makeMinesweeperBoard = (height, width, mines) => {
+  
+  let board = [];
+  
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const findSurroundingMines = (board, height, width, currX, currY) => {
+    let mineCount = 0
+    
+    let squaresToCheck = [[currX - 1, currY - 1], [currX, currY - 1],
+    [currX - 1, currY], [currX + 1, currY],
+    [currX, currY + 1], [currX + 1, currY + 1],
+    [currX - 1, currY + 1], [currX + 1, currY - 1]]
+    
+    squaresToCheck.forEach(square => {
+      if (square[0] >= 0 && square[1] >= 0 && square[0] < height && square[1] < width && board[square[0]][square[1]][0] === 'X') {
+        mineCount += 1
+      }
+    })
+    
+    return mineCount
+  }
+  
+  for (let i = 0; i < height; i++) {
+    board.push([])
+    for (let j = 0; j < width; j++) {
+      board[i].push([null, 0])
+    }
+  }
+  
+
+  let minesNeeded = mines;
+  let minesAdded = 0;
+  while (minesAdded < minesNeeded) {
+    let randomHeight = getRandomInt(height)
+    let randomWidth = getRandomInt(width)
+    if (board[randomHeight][randomWidth][0] === null) {
+      board[randomHeight][randomWidth][0] = 'X';
+      minesAdded += 1;
+      }
+  }
+  
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      let currentSquare = [i, j]
+      if (board[i][j][0] === null) {
+        board[i][j][0] = findSurroundingMines(board, height, width, i, j)
+      }
+    }
+  }
+  
+  return board
+}
+
+
+
